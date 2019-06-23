a6f8fcdb0af41268306dc0a8235af4e87f8a837d
Chupa Chuck <garyisnottooold@gmail.com>
2019-06-23 01:58:38 -0700

    Added junit calc tests

---
+++ src/test/java/core/AppTest.java
@@ -0,0 +1,11 @@
+package core;
+
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test
+
+public class AppTest {
+
+}
---
+++ src/test/java/core/JUnitTest.java
@@ -0,0 +1,9 @@
+package core;
+
+import org.junit.*;
+
+public class JunitTest {
+	
+	@BeforeClass public static void beforeClass() {System.out.println(Before Class"");}
+
+}
